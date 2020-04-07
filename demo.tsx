f4760a06d47f6f3262c9ee3cb58f208f5fdd38ea
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-04-07 22:47:22 +0800

    added label and raw json in demo page

--- demo.tsx
+++ demo.tsx
@@ -4,7 +4,9 @@ import { sampleData } from 'stencil-lib/components/json/json-common';
 import { JsonEdit } from './src/json-edit';
 import { JsonView } from './src/json-view';
 
+// tslint:disable-next-line no-unused-expression
 Surplus;
+
 S.root(() => {
   sampleData.friendNodes = [
     <span style={{ fontSize: 'larger' }}>Bob</span>,
@@ -19,19 +21,43 @@ S.root(() => {
     return <JsonView data={sampleData}/>;
   }
 
+  function raw(_signal: any) {
+    const data = { ...sampleData };
+    data.salary = '(cannot encode bigint)';
+    return <pre><code>{JSON.stringify(data, undefined, 2)}</code></pre>;
+  }
+
   let main =
     <div>
       Last update: {new Date(lastUpdate()).toLocaleString()}
       <div class='main'>
-        {view(lastUpdate())}
-        {JsonEdit({
-          data: sampleData,
-          editElement: true,
-          onChange: (ev, detail) => {
-            console.log('change', detail);
-            lastUpdate(Date.now());
-          },
-        })}
+        <div>
+          <h2>
+            JSON
+          </h2>
+          <div class='demo'>{raw(lastUpdate())}</div>
+          <h2>
+            JsonView
+          </h2>
+          <div class='demo'>
+            {view(lastUpdate())}
+          </div>
+        </div>
+        <div>
+          <h2>
+            JsonEdit
+          </h2>
+          <div class='demo'>
+            {JsonEdit({
+              data: sampleData,
+              editElement: true,
+              onChange: (ev, detail) => {
+                console.log('change', detail);
+                lastUpdate(Date.now());
+              },
+            })}
+          </div>
+        </div>
       </div>
     </div>;
   document.body.appendChild(main);
